eb7cc207e6ed05d5a55fdded69f7c6adf4f0478b
Gohana <opo7726@gmail.com>
2022-01-23 19:59:20 +0900

    Update greetings.js

--- js/greetings.js
+++ js/greetings.js
@@ -14,7 +14,7 @@ function onLoginSubmit(event) {
 }
   
 function paintGreetings(username) {
-    greeting.innerText = `Hello 🤚 ${username} 🤚`;
+    greeting.innerText = `Hello ${username}!`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
